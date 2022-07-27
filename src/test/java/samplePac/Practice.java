7e75a23358cb31946244076866a0452e2dbdcff2
Pmani498 <mani.palluru@gmail.com>
2022-07-27 16:07:25 +0530

    First Commit

---
+++ src/test/java/samplePac/Practice.java
@@ -0,0 +1,11 @@
+package samplePac;
+
+public class Practice {
+
+	public static void main(String[] args) {
+          System.out.println("First github project");
+          System.out.println("Second github project");
+          System.out.println("Third github project");
+	}
+
+}
